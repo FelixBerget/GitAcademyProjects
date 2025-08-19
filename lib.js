f40789b38a90d6739731f7a907c49ebd998268d5
FelixBerget <felixberget@gmail.com>
2025-08-19 13:26:54 +0200

    Everything in one commit since i did not set it up probperly eralier

---
+++ lib.js
@@ -0,0 +1,45 @@
+function fixText(txt){
+    textBack = ""
+    firstTime = true
+    for(let i=0;i<txt.length;i++){
+        if(txt[i]==" "){
+            
+        }
+        else{
+            if(firstTime==true){
+                textBack += txt[i].toUpperCase()
+                firstTime=false
+            } 
+            else{
+                textBack += txt[i]
+            }
+        }
+    }
+    return textBack
+}
+function emailFunc(txt){
+    let isEmail = false;
+    for(let i=0;i<txt.length;i++){
+        if(txt[i]=="@"){
+            isEmail=true
+        }
+    }
+    return isEmail
+}
+function emailFunc2(txt){
+    let hasAlpha = false;
+    let hasPunc = false;
+    let isEmail = false;
+    for(let i=0;i<txt.length;i++){
+        if(txt[i]=="@"){
+            hasAlpha=true
+        }
+        if(txt[i]=="."){
+            hasPunc=true
+        }
+    }
+    if(hasAlpha==true & hasPunc==true){
+        isEmail=true
+    }
+    return isEmail
+}
