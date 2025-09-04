9022dcb5e0bfddf7eb740caaf0c775bfe447034e
FelixBerget <felixberget@gmail.com>
2025-09-04 14:18:58 +0200

    Trying to use a script file

---
+++ usingScript.js
@@ -0,0 +1,6 @@
+let app = document.getElementById("app")
+
+app.innerHTML = `
+<div id="s">This app uses a script file</div>
+<div id="g">We are trying it with different styles</div>
+<img src="" alt="must have forgot the image"/>                `
