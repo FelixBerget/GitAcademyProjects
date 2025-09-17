df6924552ad691434aa41c36c072f9f05541668e
FelixBerget <felixberget@gmail.com>
2025-09-17 14:51:18 +0200

    Update randomDateScript.js

--- randomDateScript.js
+++ randomDateScript.js
@@ -2,7 +2,18 @@ let app = document.getElementById("app")
 
 start()
 function start(){
-    let date = new Date(randomNr(1975,2020),randomNr(0,12),randomNr(1,27),randomNr(0,24),randomNr(0,60))
+    let month = randomNr(0,12)
+    let day = 0
+    if(month == 2){
+        day = randomNr(1,27)
+    }
+    else if(month%2==0){
+        day = randomNr(1,30)
+    }
+    else{
+        day = randomNr(1,31)
+    }
+    let date = new Date(randomNr(1975,2020),month,day,randomNr(0,24),randomNr(0,60))
     console.log(date)
     app.innerHTML=date
 }
